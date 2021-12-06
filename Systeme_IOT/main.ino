f9da26ba0e09dab42df739f51f5dbeda3c796954
RIO <raphael.06rio@gmail.com>
2021-12-06 09:26:07 +0100

    Sleep ok

--- Systeme_IOT/main.ino
+++ Systeme_IOT/main.ino
@@ -6,7 +6,7 @@
 #define pinCE           7             // On associe la broche "CE" du NRF24L01 à la sortie digitale D7 de l'arduino
 #define pinCSN          8             // On associe la broche "CSN" du NRF24L01 à la sortie digitale D8 de l'arduino
 #define tunnel          "PP123"       // On définit un "nom de tunnel" (5 caractères), pour pouvoir communiquer d'un NRF24 à l'autre
-#define TIME_SLEEP_S    5             // in s
+#define TIME_SLEEP_S    1000          // in s
 #define TIME_ONE_SLEEP  1             // in s
 RF24 radio(pinCE, pinCSN);            // Instanciation du NRF24L01
 
@@ -24,7 +24,7 @@ void setup() {
   // Triggers a TIME_SLEEP_MS ms sleep (the device will be woken up only by the registered wakeup sources and by internal RTC)
   // The power consumption of the chip will drop consistently
   nb_time_go_in_sleep = TIME_SLEEP_S / TIME_ONE_SLEEP;
-  LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);  
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);  
 }
 
 void loop() {
